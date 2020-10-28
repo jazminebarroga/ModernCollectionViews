e35c93054bf42176818ebd46a02c467b9cc30e26
Jazmine Paola Barroga <jpbbarroga@gmail.com>
2020-10-28 14:20:06 +0800

    Initial Commit

---
+++ moderncollectionviews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moderncollectionviews
+//
+//  Created by Jazmine Paola Barroga on 10/28/20.
+//  Copyright © 2020 jazminebarroga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
